4592f9d8b6b536d30c0fce4f7513048aeb8a414e
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2023-04-02 18:44:05 +0200

    [UPGRADE] added build sh

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ "$1" == "start" ]]; then
+    # Build
+    echo "--- BUILDING ---"
+
+    docker build ./ -t plusone-image
+
+    echo "--- FISNIHED BUILDING ---"
+
+    # Run
+    echo "--- EXECUTING IMAGES ---"
+
+    docker run -d --name plusone -p 3000:3000/tcp plusone-image
+
+    echo "--- FINSIHED EXECUTION ---"
+
+    # Confirm
+    echo "--- DOCKER PROCESS ARE RUNNING ---"
+    echo "visit http://10.8.2.75:8080 for the results"
+elif [[ "$1" == "restart" ]]; then
+    # Build
+    echo "--- DELETING OLD PROCESSES ---"
+    docker rm -f plusone
+
+    echo "--- DELETING OLD IMAGES ---"
+    docker rmi -f plusone-image
+
+    echo "--- BUILDING ---"
+
+    docker build ./ -t plusone-image
+
+    echo "--- FISNIHED BUILDING ---"
+
+    # Run
+    echo "--- EXECUTING IMAGES ---"
+
+    docker run -d --name plusone -p 3000:3000/tcp plusone-image
+
+    echo "--- FINSIHED EXECUTION ---"
+
+    # Confirm
+    echo "--- DOCKER PROCESS ARE RUNNING ---"
+    echo "visit http://194.13.83.57:3000 for the results"
+elif [[ "$1" == "delete" ]]; then
+    echo "--- DELETING OLD PROCESSES ---"
+    docker rm -f plusone
+
+    echo "--- DELETING OLD IMAGES ---"
+    docker rmi -f plusone-image
+else
+    echo "INVALID OPTIONS"
+fi
